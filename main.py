48892e083007b68dbe8f28ca7a811c91078315bc
Jeffrey Ricker <jeffrey.ricker@gmail.com>
2018-04-01 09:58:07 -0500

    added welcome template, started verification for empty fields in form

--- main.py
+++ main.py
@@ -5,8 +5,41 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+@app.route("/add", methods = ['POST'])
+def verify_user():
+    
+    error = False
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify-password']
+#    email = request.form['email']
 
-@app.route("/")
+    if username == '' or username.strip() == '':
+        error = True
+        username_error = "Please enter a username"
+    
+    elif password == '' or password.strip() == '':
+        error = True
+        password_error = "Please enter a password"
+
+    elif verify_password == '' or verify_password.strip() == '':
+        error = True
+        verify_password_error = "Please enter a password"
+    
+    elif password and verify_password and not password == verify_password:
+        error = True
+        verify_password_error = "Password does not match"
+
+    if error:
+        return redirect("/", username_error = username_error, password_error = password_error, verify_password_error = verify_password_error)
+    else:
+        return render_template('welcome.html', username = username)
+
+
+@app.route("/", methods = ['POST', 'GET'])
 def index():
+    username_error = request.args.get("username_error")
+    password_error = request.args.get("password_error")
+    return render_template('signup.html', username_error=username_error, password_error=password_error)
 
-    return render_template('signup.html')
+app.run()
