f73078c3f860db8543036d41012bab665bda318b
Jeffrey Ricker <jeffrey.ricker@gmail.com>
2018-04-04 05:48:32 -0500

    modified input verification

--- main.py
+++ main.py
@@ -12,34 +12,52 @@ def verify_user():
     username = request.form['username']
     password = request.form['password']
     verify_password = request.form['verify-password']
-#    email = request.form['email']
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+
+#  email = request.form['email']
 
     if username == '' or username.strip() == '':
         error = True
         username_error = "Please enter a username"
     
-    elif password == '' or password.strip() == '':
+    if password == '' or password.strip() == '':
         error = True
         password_error = "Please enter a password"
 
-    elif verify_password == '' or verify_password.strip() == '':
+    if verify_password == '' or verify_password.strip() == '':
         error = True
         verify_password_error = "Please enter a password"
     
-    elif password and verify_password and not password == verify_password:
+    if not password == verify_password:
         error = True
         verify_password_error = "Password does not match"
 
     if error:
-        return redirect("/", username_error = username_error, password_error = password_error, verify_password_error = verify_password_error)
+        return render_template("signup.html", 
+            username=username,
+            username_error = username_error, 
+            password_error = password_error, 
+            verify_password_error = verify_password_error)
     else:
         return render_template('welcome.html', username = username)
 
 
-@app.route("/", methods = ['POST', 'GET'])
-def index():
-    username_error = request.args.get("username_error")
-    password_error = request.args.get("password_error")
-    return render_template('signup.html', username_error=username_error, password_error=password_error)
+@app.route("/", methods = ['POST'])
+#def validate_user():
+#    username_error = request.args.get("username_error")
+#    password_error = request.args.get("password_error")
+#    verify_password_error = request.args.get("verify_password_error")
 
+#    return render_template('signup.html', 
+#        username_error=username_error, 
+#        password_error=password_error, 
+#        verify_password_error=verify_password_error)
+
+@app.route("/")
+def display_signup_form():
+    return render_template('signup.html')
+    
+    
 app.run()
